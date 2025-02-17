12d2991616bcb6f74caddd04b98c8dfd30610316
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-17 20:51:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,27 @@
+# terraform/main.tf
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"  # Make sure you have cluster credentials
+# Get GKE cluster info
+data "google_container_cluster" "my_cluster" {
+  name     = "autopilot-cluster-1-test"  # This should be your actual GKE cluster name
+  location = var.region
 }
+
+# Configure Kubernetes provider with GKE authentication
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+data "google_client_config" "default" {}
 
 resource "kubernetes_deployment" "flask_app" {
   metadata {
-    name = "flask-app"
+    name      = "flask-app"
     namespace = "default"
   }
 
@@ -79,5 +91,34 @@ resource "kubernetes_deployment" "flask_app" {
         max_unavailable = "25%"
       }
     }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to these annotations as they are managed by GKE
+      metadata[0].annotations["autopilot.gke.io/resource-adjustment"],
+      metadata[0].annotations["autopilot.gke.io/warden-version"]
+    ]
+  }
+}
+
+# Service to expose the application
+resource "kubernetes_service" "flask_app" {
+  metadata {
+    name      = "flask-app"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.flask_app.spec[0].template[0].metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
   }
 }
