5ec0fb047ed4a9b3f6aa055526ca8d498a9211d4
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-17 20:25:17 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,16 @@
-output "cluster_endpoint" {
-  description = "GKE cluster endpoint"
-  value       = google_container_cluster.primary.endpoint
+# terraform/outputs.tf
+
+output "deployment_status" {
+  description = "Status of the flask app deployment"
+  value = {
+    name              = kubernetes_deployment.flask_app.metadata[0].name
+    namespace         = kubernetes_deployment.flask_app.metadata[0].namespace
+    current_image     = kubernetes_deployment.flask_app.spec[0].template[0].spec[0].container[0].image
+    desired_replicas  = kubernetes_deployment.flask_app.spec[0].replicas
+  }
 }
 
-output "cluster_ca_certificate" {
-  description = "GKE cluster CA certificate"
-  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-  sensitive   = true
+output "image_version" {
+  description = "Current container image being used"
+  value       = var.app_image
 }
